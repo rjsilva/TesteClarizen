b7c2b5432a5adf44e140236862ff68ba8b38a6f8
yamanuser <yamanuser@note_pe01nbuz>
2017-04-10 13:20:11 -0300

    comitei agora

--- src/main/java/br/com/modelo/Teste.java
+++ src/main/java/br/com/modelo/Teste.java
@@ -9,7 +9,7 @@ public class Teste {
 	
 	public static void main(String[] args) {
 		
-
+        //testa
 		Teste teste = new Teste();
 		teste.logar();
 	}
@@ -19,12 +19,13 @@ public class Teste {
 		
 		System.setProperty("webdriver.chrome.driver", "C:/Users/yamanuser/chromedriver.exe");
 		
+		//acessa git hub
 		WebDriver drive = new ChromeDriver();
 		drive.manage().window().maximize();
 		drive.get("https://app2.clarizen.com/Clarizen/Pages/Service/Login.aspx?ReturnUrl=%2fClarizen%2fTeamSpace%2fTimesheet");
 		
 		drive.findElement(By.id("txtLogin")).sendKeys("ronaldo.silva@yaman.com.br");
-		drive.findElement(By.id("txtPassword")).sendKeys("wx0985a1986");
+		drive.findElement(By.id("txtPassword")).sendKeys("wx0985a1983");
 		drive.findElement(By.id("lbtLogin")).click();
 	}
 }
